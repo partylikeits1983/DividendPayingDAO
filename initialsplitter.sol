d3c6890b10c0eb8a5bdf9fa0045d673723d35505
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-01 16:27:14 +0300

    Update initialsplitter.sol

--- initialsplitter.sol
+++ initialsplitter.sol
@@ -7,23 +7,20 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract PaymentSplitter is Context {
 
-
-    
     uint256 private _totalReleased;
-    
     mapping(address => uint256) private _released;
     address[] private _payees;
-
 
    /**
     * all my crap
     */
 
     mapping(address => uint256) private _balances;
-    address private _tokenaddress;
+    address payable private _tokenaddress;
     address private _owner;
     uint256 private _fee;
-
+    
+    uint256 public dividend;
 
 
     constructor(uint256 fee_) {
@@ -33,22 +30,18 @@ contract PaymentSplitter is Context {
 
 
 
-    function totalReleased() public view returns (uint256) {
-        return _totalReleased;
+
+    function transfer(address recipient) public payable {
+        _balances[recipient]+=msg.value;
     }
-
-
-    function released(address account) public view returns (uint256) {
-        return _released[account];
-    }
-
-
 
 
     function release(address payable account) public virtual {
 
         uint256 totalReceived = address(this).balance + _totalReleased;
         uint256 payment = (totalReceived * (100 - _fee)) / 100 - _released[account];
+        
+        dividend = (totalReceived * _fee) / 100 - _released[_tokenaddress];
 
         require(payment != 0, "PaymentSplitter: account is not due payment");
 
@@ -60,11 +53,24 @@ contract PaymentSplitter is Context {
     }
 
 
-    function updeateTokenAddress(address account) public {
+
+    function payDividend() public virtual {
+
+        require(dividend != 0, "PaymentSplitter: account is not due payment");
+
+        _released[_tokenaddress] = _released[_tokenaddress] + dividend;
+        _totalReleased = _totalReleased + dividend;
+
+        Address.sendValue(_tokenaddress, dividend);
+
+    }
+
+
+    function updeateTokenAddress(address payable account) public {
         require(msg.sender == _owner, "Only the owner");
         _tokenaddress = account;
-        
     }
+    
     
     function updeateFee(uint256 fee) public {
         require(msg.sender == _owner, "Only the owner");
@@ -72,6 +78,9 @@ contract PaymentSplitter is Context {
     }
     
     
+    function getfee() public view returns (uint256) {
+        return _fee;
+    }
     
     function owner() public view returns (address) {
         return _owner;
@@ -83,28 +92,32 @@ contract PaymentSplitter is Context {
     }
 
 
-        
-    function transfer(address recipient) public payable {
-        
-        _balances[recipient]+=msg.value;
 
-    }
-    
-    
-    
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
     
     
+    function getDividend() public view returns(uint256){
+        return dividend;
+    }
+    
+
     
     function getUserBalance(address account) public view returns(uint256){
         return _balances[account];
     }
+    
+    
+    function totalReleased() public view returns (uint256) {
+        return _totalReleased;
+    }
+
+
+    function released(address account) public view returns (uint256) {
+        return _released[account];
+    }
  
-    
-    
-    
     
 }
 
