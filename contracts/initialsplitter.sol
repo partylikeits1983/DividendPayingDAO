1c50e9a13219793770faeb476dd02f96d289d050
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-04 09:46:20 +0300

    Update initialsplitter.sol

--- contracts/initialsplitter.sol
+++ contracts/initialsplitter.sol
@@ -2,47 +2,249 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 
-contract PaymentSplitter is Context {
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+
+contract ERC20 is Context, IERC20, IERC20Metadata {
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+    
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+      
+    event PayeeAdded(address account, uint256 shares);
+    event PaymentReleased(address to, uint256 amount);
+    event PaymentReceived(address from, uint256 amount);
+    
+    mapping(address=>uint) ethbalance;
+    
     uint256 private _totalReleased;
+    
     mapping(address => uint256) private _released;
     address[] private _payees;
 
-   /**
-    * all my crap
-    */
+    constructor(
+        string memory name_, 
+        string memory symbol_, 
+        uint supply,
+        address[] memory payees, 
+        uint256[] memory amount) payable {
+            
+        _name = name_;
+        _symbol = symbol_;
+        _mint(msg.sender, supply);
+        require(payees.length == amount.length, "PaymentSplitter: payees and shares length mismatch");
+        require(payees.length > 0, "PaymentSplitter: no payees");
+        
+        for (uint256 i = 0; i < payees.length; i++) {
+            _addPayee(payees[i], amount[i]);
+        }
+      
+    }
 
-    mapping(address => uint256) private _balances;
-    address payable private _tokenaddress;
-    address private _owner;
-    uint256 private _fee;
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
     
-    uint256 private dividend;
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
 
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        unchecked {
+            _approve(sender, _msgSender(), currentAllowance - amount);
+        }
 
-    constructor(uint256 fee_) {
-        _owner = msg.sender;
-        _fee = fee_;
+        return true;
     }
 
-
-
-    function transfer(address recipient) public payable {
-        _balances[recipient]+=msg.value;
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
+        return true;
     }
 
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        uint256 currentAllowance = _allowances[_msgSender()][spender];
+        require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
+        unchecked {
+            _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        }
 
+        return true;
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+  
+        }
+        
+        _balances[recipient] += amount;
+        _payees.push(recipient);
+        
+        emit Transfer(sender, recipient, amount);
+        _afterTokenTransfer(sender, recipient, amount);
+        
+        //** adds new account to _payees array and how many 
+        _payees.push(recipient);
+        _balances[recipient] = amount;
+
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+
+        _afterTokenTransfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+
+        _afterTokenTransfer(account, address(0), amount);
+    }
+
+    function _approve(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual {}
+    
+    
+    receive() external payable virtual {
+        emit PaymentReceived(_msgSender(), msg.value);
+    }
+    
+    
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+    
+    /**
+     * @dev Getter for the total amount of Ether already released.
+     */
+    function totalReleased() public view returns (uint256) {
+        return _totalReleased;
+    }
+
+    /**
+     * @dev Getter for the amount of shares held by an account.
+     */
+    function shares(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev Getter for the amount of Ether already released to a payee.
+     */
+    function released(address account) public view returns (uint256) {
+        return _released[account];
+    }
+
+    /**
+     * @dev Getter for the address of the payee number `index`.
+     */
+    function payee(uint256 index) public view returns (address) {
+        return _payees[index];
+    }
+
+    /**
+     * @dev Triggers a transfer to `account` of the amount of Ether they are owed, according to their percentage of the
+     * total shares and their previous withdrawals.
+     */
     function release(address payable account) public virtual {
-        require(_tokenaddress != account, "PaymentSplitter: Account cannot be token address");
-        require(_balances[account] > 0, "PaymentSplitter: account balance is 0");
-        
+        require(_balances[account] > 0, "PaymentSplitter: account has no shares");
+
         uint256 totalReceived = address(this).balance + _totalReleased;
-        uint256 payment = (_balances[account] * (100 - _fee)) / 100;
-        
-        dividend = (totalReceived * _fee) / 100 - _released[_tokenaddress];
+        uint256 payment = (totalReceived * _balances[account]) / _totalSupply - _released[account];
 
         require(payment != 0, "PaymentSplitter: account is not due payment");
 
@@ -50,68 +252,18 @@ contract PaymentSplitter is Context {
         _totalReleased = _totalReleased + payment;
 
         Address.sendValue(account, payment);
+        emit PaymentReleased(account, payment);
     }
 
+    function _addPayee(address account, uint256 amount) private {
+        require(account != address(0), "PaymentSplitter: account is the zero address");
+        require(amount > 0, "PaymentSplitter: shares are 0");
+        /** require(_balances[account] == 0, "PaymentSplitter: account already has shares"); **/
 
-    function releaseDividend() public virtual {
-        require(dividend != 0, "PaymentSplitter: account is not due payment");
-
-        _released[_tokenaddress] = _released[_tokenaddress] + dividend;
-        _totalReleased = _totalReleased + dividend;
-
-        Address.sendValue(_tokenaddress, dividend);
+        _payees.push(account);
+        _balances[account] = amount;
+        
+        emit PayeeAdded(account, amount);
     }
-
-
-    function updeateTokenAddress(address payable account) public {
-        require(msg.sender == _owner, "Only the owner");
-        _tokenaddress = account;
-    }
-    
-    
-    function updeateFee(uint256 fee) public {
-        require(msg.sender == _owner, "Only the owner");
-        require(fee <= 10, "Fee must not be higher than 10%");
-        _fee = fee;
-    }
-    
-    
-    function getfee() public view returns (uint256) {
-        return _fee;
-    }
-    
-    function owner() public view returns (address) {
-        return _owner;
-    }
-    
-    
-    function tokenAddress() public view returns (address) {
-        return _tokenaddress;
-    }
-
-
-    function getBalance() public view returns(uint){
-        return address(this).balance;
-    }
-    
-    
-    function getDividend() public view returns(uint256){
-        return dividend;
-    }
-    
-    
-    function getUserBalance(address account) public view returns(uint256){
-        return _balances[account];
-    }
-    
-    
-    function totalReleased() public view returns (uint256) {
-        return _totalReleased;
-    }
-
-
-    function released(address account) public view returns (uint256) {
-        return _released[account];
-    }
- 
 }
+
