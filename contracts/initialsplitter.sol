6b554c9e0b5360db401a5e12995f63c82b40a7ed
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-17 12:19:54 +0300

    Create initialsplitter.sol

---
+++ contracts/initialsplitter.sol
@@ -0,0 +1,76 @@
+
+
+contract PaymentSplitter is Context {
+
+    uint256 private _totalReleased;
+    mapping(address => uint256) private _released;
+    address[] private _payees;
+    
+    uint256 private _fee = 5;
+    address private _owner = msg.sender;
+
+    mapping(address => uint256) private _balances;
+    
+    uint256 private dividend;
+
+    address _tokenaddress = address(this);
+
+
+
+    function transfer(address recipient) public payable {
+        _balances[recipient]+=msg.value;
+    }
+
+
+    function releaseinit(address payable account) public virtual {
+        
+        require(_balances[account] > 0, "PaymentSplitter: account balance is 0");
+        
+        uint256 totalReceived = address(this).balance + _totalReleased;
+        uint256 payment = (_balances[account] * (100 - _fee)) / 100;
+        
+        dividend = (totalReceived * _fee) / 100 - _released[_tokenaddress];
+
+        require(payment != 0, "PaymentSplitter: account is not due payment");
+
+        _released[account] = _released[account] + payment;
+        _totalReleased = _totalReleased + payment;
+
+        Address.sendValue(account, payment);
+    }
+
+
+    
+    function updeateFee(uint256 fee) public {
+        require(msg.sender == _owner, "Only the owner");
+        require(fee <= 10, "Fee must not be higher than 10%");
+        _fee = fee;
+    }
+    
+    
+    function getfee() public view returns (uint256) {
+        return _fee;
+    }
+    
+    
+    function owner() public view returns (address) {
+        return _owner;
+    }
+    
+    
+    function getUserBalance(address account) public view returns(uint256){
+        return _balances[account];
+    }
+    
+    
+    function totalReleasedinit() public virtual returns (uint256) {
+        return _totalReleased;
+    }
+
+
+    function releasedinit(address account) public virtual returns (uint256) {
+        return _released[account];
+    }
+ 
+}
+
