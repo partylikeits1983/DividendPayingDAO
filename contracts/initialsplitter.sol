22b9fd74d0e1cf36af6c9a64deba33a41ad4f6d3
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-17 12:23:47 +0300

    Update initialsplitter.sol

--- contracts/initialsplitter.sol
+++ contracts/initialsplitter.sol
@@ -1,9 +1,3 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 
 contract PaymentSplitter is Context {
 
